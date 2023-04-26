7a8744716d4c9c898680df424e11c2b5c214888e
SuryaVelmurugesan <suryavelmurugesan@gmail.com>
2023-04-26 12:16:04 +0530

    initial commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0xbD171d90EFBd63e382aD27EDFDfb87FD5A198701";
+export const contractABI = abi.abi;
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,10 @@
+// https://eth-goerli.g.alchemy.com/v2/74HhzLNpAFeoaryMU4J3uJBwFM2fULYL
+import '@nomiclabs/hardhat-waffle';
+
+export const solidity = '0.8.0';
+export const networks = {
+  ropsten: {
+    url: 'https://eth-goerli.g.alchemy.com/v2/74HhzLNpAFeoaryMU4J3uJBwFM2fULYL',
+    accounts: ['48d38bf0c69c08871416c60db67380d05314228ffbcff724867333743bf85abf'],
+  },
+};
